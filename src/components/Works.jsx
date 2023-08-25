66c716583e65d76c8de4908823e97ff2ca4c064a
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-08-25 18:48:26 +0300

    title edit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -37,7 +37,7 @@ function Works() {
           />
         ))}
       </div> */}
-      <div className="works-title">Android Apps</div>
+      <div className="works-title">Mobile Apps</div>
       <div className="cards">
         {apps.map((item) => (
           <CardMob
